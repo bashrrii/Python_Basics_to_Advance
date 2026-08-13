ec073cdb982625433103675cdaccf3001ca7791b
bashrrii <mubbashir1434@gmail.com>
2026-08-13 11:21:06 +0500

    1. basic variables

---
+++ 9-variable.py
@@ -0,0 +1,14 @@
+a = 10
+
+
+# in memory location a memory is assigned whose name is a. and we place 10 in that memory location
+
+
+b=5
+
+print(a)
+print(b)
+
+
+print("a is at it is")
+print("b      is as it is")
