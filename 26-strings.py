a78036c9e84f9c293abc4c9354daa9ac28a9523b
bashrrii <mubbashir1434@gmail.com>
2026-08-25 12:21:59 +0500

    string arrays,length of string,check string

---
+++ 26-strings.py
@@ -0,0 +1,31 @@
+# Get the character at position 1 (remember that the first character has the position 0):
+
+a = "Hello, World!"
+
+
+# indexing in python 0,1,2,3,4,5.... or negative indexing -1,-2,-3,-4,-5,...
+
+b= a[0]
+print(b)
+
+
+print(a[4])
+
+
+print(a[-1])
+
+print(a[6])
+
+print("================================")
+a = "Hello, World!"
+# find length
+
+length_of_variable = len(a)
+print(length_of_variable)
+
+
+# in operator will return true or false
+# Check if "free" is present in the following text:
+txt = "The best things in life are free!"
+result = "free" in txt
+print(result)
