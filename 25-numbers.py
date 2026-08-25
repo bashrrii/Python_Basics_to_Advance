4aca658289c249466d53e42c55ffd0513df2e116
bashrrii <mubbashir1434@gmail.com>
2026-08-25 12:18:35 +0500

    numbers

---
+++ 25-numbers.py
@@ -0,0 +1,33 @@
+a =5
+b =10.90
+z = 1j
+
+type_of_a = type(a)
+print(type_of_a)
+
+
+
+print(type(b))
+print(type(b))
+
+
+print(type(z))
+
+
+
+a = float(a)
+print(type(a))
+print(a)
+
+
+b = int(b)
+print(type(b))
+print(b)
+
+
+
+
+
+import random
+chakka = random.randrange(1, 6)
+print(chakka)
