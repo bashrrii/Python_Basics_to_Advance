4b635dc692ce40fe83cf397d94526a42b690e446
bashrrii <mubbashir1434@gmail.com>
2026-08-25 11:42:29 +0500

    statements and usage of  (end= " ")

---
+++ 16-statement.py
@@ -0,0 +1,19 @@
+print("jjjjjjjjjjjjjjjjjjjjjjjjj")
+
+print('kkkkkkkkkkkkkkk')
+
+a= 444
+print(a)
+
+
+print('a')
+
+
+
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+
+print("I am", 35, "years old.")
