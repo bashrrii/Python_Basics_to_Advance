5c511680b9712a2d36d036c0d133e26116e6f522
bashrrii <mubbashir1434@gmail.com>
2026-08-25 12:03:39 +0500

    casting

---
+++ 21-casting.py
@@ -0,0 +1,38 @@
+a = 10.8
+
+type_of_a  = type(a)
+print(type_of_a)
+
+
+b = 45
+type_of_b = type(b)
+print(type_of_b)
+
+
+
+c= "pakistan"
+type_of_c = type(c)
+print(type_of_c)
+
+
+
+d = str(5)
+type_of_d = type(d)
+print(type_of_d)
+
+
+
+
+z = float(3)
+print(type(z))
+print(z)
+
+
+
+
+# python is case sensitive
+A=12
+A=87
+
+
+a=90
