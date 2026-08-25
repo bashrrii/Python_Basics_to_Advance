6001cc8e98aef958405396db5ba513773b3af90b
bashrrii <mubbashir1434@gmail.com>
2026-08-25 11:49:53 +0500

    2. input

---
+++ 19-input.py
@@ -0,0 +1,34 @@
+
+
+
+a = input("enter 1st number     ")
+b = input("enter 2nd number      ")
+
+
+
+a = int(a)
+b = int(b)
+
+
+result = (a+b)
+print(result)
+
+
+result = (a-b)
+print(result)
+
+
+result = (a*b)
+print(result)
+
+
+result = (a/b)
+print(result)
+
+
+
+
+
+
+
+
