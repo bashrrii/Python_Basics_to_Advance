070e0c3d676293d3450a5b3327f160c93b9403a0
bashrrii <mubbashir1434@gmail.com>
2026-08-25 12:23:46 +0500

    for loop

---
+++ 27-if_and_for.py
@@ -0,0 +1,21 @@
+# Loop through the letters in the word "banana":
+a = "banana"
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+
+
+# for i in string:
+for i in a:
+   print(i)
+
+
+password_key= "apple"
+password = ""
+for i in password_key:
+    i=i+ "k"
+    password = password + i
+print(password)
