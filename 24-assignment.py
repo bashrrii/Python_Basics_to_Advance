f1261cf31b0cfe203d7b189d93dcd9f6c0dab9b3
bashrrii <mubbashir1434@gmail.com>
2026-08-25 12:10:01 +0500

    3. input

---
+++ 24-assignment.py
@@ -0,0 +1,81 @@
+# a = 2
+# b = 5
+
+
+name_1 = input("please enter name of first employ   ")
+employ_id_1= input("please enter emply id of first employ    ")
+dept_1 = input("please enter department of first employ   ")
+job_title_1 = input("please enter job title of first employ   ")
+age_1 = input("please enter age of first employ   ")
+sal_1 = input("please enter salary of first employ   ")
+exp_1 = input("please enter experience of first employ   ")
+
+
+
+name_2 = input("please enter name of 2nd employ   ")
+employ_id_2= input("please enter emply id of 2nd employ    ")
+dept_2 = input("please enter department of 2nd employ   ")
+job_title_2 = input("please enter job title of 2nd employ   ")
+age_2 = input("please enter age of 2nd employ   ")
+sal_2 = input("please enter salary of 2nd employ   ")
+exp_2 = input("please enter experience of 2nd employ   ")
+
+
+
+name_3 = input("please enter name of 3rd employ   ")
+employ_id_3= input("please enter emply id of 3rd employ    ")
+dept_3 = input("please enter department of 3rd employ   ")
+job_title_3 = input("please enter job title of 3rd employ   ")
+age_3 = input("please enter age of 3rd employ   ")
+sal_3 = input("please enter salary of 3rd employ   ")
+exp_3 = input("please enter experience of 3rd employ   ")
+
+
+
+
+print("=======================================================")
+print("COMPANY EMPLOYER DATABASE ")
+print("=======================================================")
+
+print("Employee 1")
+
+print(".......................")
+
+print("Name        : ",name_1)
+print("Employee ID : ",employ_id_1)
+print("Department  : ",dept_1)
+print("Job title   : ",job_title_1)
+print("Age         : ",age_1)
+print("Salary      : ",sal_1)
+print("Experience  : ",exp_1)
+
+print("Employee 2")
+
+print(".......................")
+
+print("Name        : ",name_2)
+print("Employee ID : ",employ_id_2)
+print("Department  : ",dept_2)
+print("Job title   : ",job_title_2)
+print("Age         : ",age_2)
+print("Salary      : ",sal_2)
+print("Experience  : ",exp_2)
+
+
+print("Employee 2")
+
+print(".......................")
+
+print("Name        : ",name_3)
+print("Employee ID : ",employ_id_3)
+print("Department  : ",dept_3)
+print("Job title   : ",job_title_3)
+print("Age         : ",age_3)
+print("Salary      : ",sal_3)
+print("Experience  : ",exp_3  )
+
+
+
+
+
+
