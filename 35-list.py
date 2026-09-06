99a64e050841e4e56c53175345abf096c648d37f
bashrrii <mubbashir1434@gmail.com>
2026-09-06 19:31:19 +0500

    basics of list

---
+++ 35-list.py
@@ -0,0 +1,20 @@
+# Create a List:
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+
+print(type(thislist))
+
+
+thislist = [1, 3, 77]
+print(thislist)
+print(type(thislist))
+
+
+# Lists allow duplicate values:
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+# Print the number of items in the list:
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
