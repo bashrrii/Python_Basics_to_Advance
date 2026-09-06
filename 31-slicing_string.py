67c88ea82081a4a2960846bff4729084c1dcbc23
bashrrii <mubbashir1434@gmail.com>
2026-09-06 19:16:33 +0500

    slicing of stringg

---
+++ 31-slicing_string.py
@@ -0,0 +1,18 @@
+# Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+var = b[0:5]
+print(var)
+
+var2 = b[7:13]
+
+print(var2)
+
+
+b = "Hello, World!jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjssssssssssssssssssssssssssssssssiiiiiiiiiiiiiiiiiiiiiii"
+print(b[7:])
+print(b[:])
+
+print(b[5:])
+
+
+print(b[-5:-3])
