9c4e4f0d3dfc8181a5843a32e2c30e2f4b714e08
bashrrii <mubbashir1434@gmail.com>
2026-09-06 19:20:52 +0500

    how to upper,lower and split a string

---
+++ 32-string_modification.py
@@ -0,0 +1,13 @@
+# Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+b= b.upper()
+print(b)
+# The lower() method returns the string in lower case:
+
+a = "Hello, World!"
+print(a.lower())
+
+# The split() method splits the string into substrings if it finds instances of the separator:
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
