74dc5793a58e7c42b64bf79118e19312581b2bf3
bashrrii <mubbashir1434@gmail.com>
2026-09-06 19:15:53 +0500

    if statement

---
+++ 30-if_statement.py
@@ -0,0 +1,11 @@
+# if condition(True, False):
+#     do something here
+
+
+a = 100
+
+if a > 10:
+    print(a)
+    print("jsdfjkdsjfkjdsfjsdaklfladksfmlkdsjlfk")
+
+print("this is outside if condion")
