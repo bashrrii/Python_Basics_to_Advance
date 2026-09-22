ae6f1b0b82073f2a62bd0da71053c42ec3de2668
bashrrii <mubbashir1434@gmail.com>
2026-09-22 13:59:56 +0500

    for loop in list

---
+++ 38-for_loop_list.py
@@ -0,0 +1,15 @@
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):  ## 0 1 2
+  print(thislist[i])
+
+
+thislist = "apple"
+for x in thislist:
+  print(x)
+
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+
