341b1b144f430049b751da3002816091dda778d3
bashrrii <mubbashir1434@gmail.com>
2026-09-23 09:44:00 +0500

    How to access item in list?

---
+++ 54-access_item_in-set.py
@@ -0,0 +1,19 @@
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+# Check if "banana" is present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+# Check if "banana" is NOT present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
+
+
