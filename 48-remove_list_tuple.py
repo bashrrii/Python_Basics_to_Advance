01cdf1336dac6150f593755f7e10b57c30b68ab9
bashrrii <mubbashir1434@gmail.com>
2026-09-23 09:35:13 +0500

    How to remove item to tuple?

---
+++ 48-remove_list_tuple.py
@@ -0,0 +1,14 @@
+# Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+
+# The del keyword can delete the tuple completely:
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+
