9477ee339e6300a11b27fb542c2878bc359df1b6
bashrrii <mubbashir1434@gmail.com>
2026-09-23 09:34:32 +0500

    How to add item to tuple?

---
+++ 47-add_item_to_tuple.py
@@ -0,0 +1,30 @@
+# You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+# Convert the tuple into a list to be able to change it:
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+# Convert the tuple into a list, add "orange", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+
+# Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+
+
+
