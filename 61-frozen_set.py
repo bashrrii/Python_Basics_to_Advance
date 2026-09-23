2e863733a8f9a99660b0f6a23362d9c14a9806f6
bashrrii <mubbashir1434@gmail.com>
2026-09-23 19:29:07 +0500

    frozen set.

---
+++ 61-frozen_set.py
@@ -0,0 +1,14 @@
+# frozenset is an immutable version of a set.
+# Like sets, it contains unique, unordered, unchangeable elements.
+# Unlike sets, elements cannot be added or removed from a frozenset.
+
+
+# Use the frozenset() constructor to create a frozenset from any iterable.
+
+# Create a frozenset and check its type:
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+
+
